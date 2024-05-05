87ba8d8b55f35bfc1515d0fb34405e44cd184254
sakamoto-family-smile <sakamoto.family.smile@gmail.com>
2024-05-05 22:42:56 +0900

    implement chat page

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
+String randomString() {
+  final random = Random.secure();
+  final values = List<int>.generate(16, (i) => random.nextInt(255));
+  return base64UrlEncode(values);
+}
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('チャット'),
-      ),
-      body: const Center(
-          child: Text('TODO: チャット画面を実装', style: TextStyle(fontSize: 32.0))),
+  ChatScreenState createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Chat(
+          user: _user,
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
+        ),
+      );
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
     );
+
+    _addMessage(textMessage);
   }
 }
